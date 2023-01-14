40d6f4fb86fef2ba900750decf1b275b2921a82d
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2023-01-14 22:15:18 +0800

    delete

--- index.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { exec } from 'node:child_process';
-import lava from './package.json' assert { type: 'json' };
-
-exec(`npx rimraf ./dist`, (err) => {
-	if (err) return console.error(err);
-	exec(`npm install typescript && npx tsc --project ./src/tsconfig.json && ${lava.scripts.start}`, console.log);
-});
