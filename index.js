aaea6d8b5511e29a99f0063be14fa93c95ecfd47
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2023-01-14 18:07:54 +0800

    .

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
 import { exec } from 'node:child_process';
 import lava from './package.json' assert { type: 'json' };
 
-exec(`npx ${lava.scripts.build}`, console.log);
+exec(`npx ${lava.scripts.build}`, (err) => {
+	if (err) return console.error(err);
+});
