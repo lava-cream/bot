5820be14e6cbe1210d631e56ba6ec32814a952a9
BrianWasTaken <abdg2006mhpnhs@gmail.com>
2023-01-14 18:09:42 +0800

    .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import { exec } from 'node:child_process';
 import lava from './package.json' assert { type: 'json' };
 
-exec(`npx ${lava.scripts.build}`, (err) => {
+exec(`npx rimraf ./dist`, (err) => {
 	if (err) return console.error(err);
+	exec(`npx tsc --project tsconfig.json`, console.log);
 });
